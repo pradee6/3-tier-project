89c8a377a9ed83e1f88af860721dbec05bf9ee6c
Pradeep <69085522+pradee6@users.noreply.github.com>
2024-12-04 19:08:21 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify
 from flask_mail import Mail, Message
 from flask_cors import CORS  # Import CORS
-from flask_session import Session 
 import random
 import logging
 from datetime import datetime
