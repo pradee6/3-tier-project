fd4a672474f88456afc41d110537ce438ec7437e
E Pradeep <69085522+pradee6@users.noreply.github.com>
2026-02-05 19:50:43 +0530

    Update script.js

--- web/script.js
+++ web/script.js
@@ -18,7 +18,7 @@ document.getElementById("signupForm")?.addEventListener("submit", function (e) {
         phone = '+91' + phone;
     }
 
-    fetch('http://15.168.13.210:5000/signup', {
+    fetch('http://34.229.0.110:5000/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, email, password, phone, gender, dob })
